f57cd4dcb2a9b63eb4e82ec54e7839739ca1a270
MattShilling <shillinm@oregonstate.edu>
2020-05-21 18:20:20 -0700

    Removing old ugly bash script.

--- make.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# I tried so hard to get CMake working but :(
-
-/usr/local/apps/cuda/cuda-10.1/bin/nvcc -o CudaCarlo cuda_carlo.cu -std=c++11 -I/usr/local/apps/cuda/cuda-10.1/samples/common/inc/
